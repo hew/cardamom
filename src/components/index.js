ccde38cbdf3743a3570addd214c21bd989b12896
Matthew Jones <matt@hew.tools>
2018-05-22 23:59:04 -0700

    Converts product page.

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export FullWidth from './FullWidth'
+export Pricing from './Pricing'
+export Testimonials from './Testimonials'
+export Features from './Features'
--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -1,8 +1,6 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Features from '../components/Features'
-import Testimonials from '../components/Testimonials'
-import Pricing from '../components/Pricing'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {FullWidth, Pricing, Testimonials, Features} from '../components';
 
 export const ProductPageTemplate = ({
   image,
@@ -13,97 +11,65 @@ export const ProductPageTemplate = ({
   main,
   testimonials,
   fullImage,
-  pricing,
+  pricing
 }) => (
-  <section className="section section--gradient">
+  <section>
     <Container>
-      <div className="section">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="content">
-              <div
-                className="full-width-image-container margin-top-0"
-                style={{ backgroundImage: `url(${image})` }}
-              >
-                <h2
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                    padding: '1rem',
-                  }}
-                >
-                  {title}
-                </h2>
-              </div>
-              <div className="columns">
-                <div className="column is-7">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    {heading}
-                  </h3>
-                  <p>{description}</p>
-                </div>
-              </div>
-              <Features gridItems={intro.blurbs} />
-              <div className="columns">
-                <div className="column is-7">
-                  <h3 className="has-text-weight-semibold is-size-3">
-                    {main.heading}
-                  </h3>
-                  <p>{main.description}</p>
-                </div>
-              </div>
-              <div className="tile is-ancestor">
-                <div className="tile is-vertical">
-                  <div className="tile">
-                    <div className="tile is-parent is-vertical">
-                      <article className="tile is-child">
-                        <img
-                          style={{ borderRadius: '5px' }}
-                          src={main.image1.image}
-                          alt={main.image1.alt}
-                        />
-                      </article>
-                    </div>
-                    <div className="tile is-parent">
-                      <article className="tile is-child">
-                        <img
-                          style={{ borderRadius: '5px' }}
-                          src={main.image2.image}
-                          alt={main.image2.alt}
-                        />
-                      </article>
-                    </div>
-                  </div>
-                  <div className="tile is-parent">
-                    <article className="tile is-child">
-                      <img
-                        style={{ borderRadius: '5px' }}
-                        src={main.image3.image}
-                        alt={main.image3.alt}
-                      />
-                    </article>
-                  </div>
-                </div>
-              </div>
-              <Testimonials testimonials={testimonials} />
-              <div
-                className="full-width-image-container"
-                style={{ backgroundImage: `url(${fullImage})` }}
-              />
-              <h2 className="has-text-weight-semibold is-size-2">
-                {pricing.heading}
-              </h2>
-              <p className="is-size-5">{pricing.description}</p>
-              <Pricing data={pricing.plans} />
-            </div>
-          </div>
-        </div>
-    </div>
-      </Container>
+      <FullWidth style={{backgroundImage: `url(${image})`}}>
+        <H2
+          style={{
+            boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+            backgroundColor: '#f40',
+            color: 'white',
+            padding: '1rem'
+          }}>
+          {title}
+        </H2>
+      </FullWidth>
+          <H3>{heading}</H3>
+          <P>{description}</P>
+      <Features gridItems={intro.blurbs} />
+      <Box>
+        <h3 className="has-text-weight-semibold is-size-3">{main.heading}</h3>
+        <p>{main.description}</p>
+      </Box>
+      <Row>
+        <Box width={[1, 1 / 2]}>
+          <article>
+            <img
+              style={{borderRadius: '5px'}}
+              src={main.image1.image}
+              alt={main.image1.alt}
+            />
+          </article>
+        </Box>
+        <Box width={[1, 1 / 2]}>
+          <article>
+            <img
+              style={{borderRadius: '5px'}}
+              src={main.image2.image}
+              alt={main.image2.alt}
+            />
+          </article>
+        </Box>
+        <Box width={[1, 1 / 2]}>
+          <article>
+            <img
+              style={{borderRadius: '5px'}}
+              src={main.image3.image}
+              alt={main.image3.alt}
+            />
+          </article>
+        </Box>
+      </Row>
+      <Testimonials testimonials={testimonials} />
+      <FullWidth style={{backgroundImage: `url(${fullImage})`}} />
+      <h2 className="has-text-weight-semibold is-size-2">{pricing.heading}</h2>
+      <p className="is-size-5">{pricing.description}</p>
+      <Pricing data={pricing.plans} />
+    </Container>
   </section>
-)
+);
 
 ProductPageTemplate.propTypes = {
   image: PropTypes.string,
@@ -111,26 +77,26 @@ ProductPageTemplate.propTypes = {
   heading: PropTypes.string,
   description: PropTypes.string,
   intro: PropTypes.shape({
-    blurbs: PropTypes.array,
+    blurbs: PropTypes.array
   }),
   main: PropTypes.shape({
     heading: PropTypes.string,
     description: PropTypes.string,
     image1: PropTypes.object,
     image2: PropTypes.object,
-    image3: PropTypes.object,
+    image3: PropTypes.object
   }),
   testimonials: PropTypes.array,
   fullImage: PropTypes.string,
   pricing: PropTypes.shape({
     heading: PropTypes.string,
     description: PropTypes.string,
-    plans: PropTypes.array,
-  }),
-}
+    plans: PropTypes.array
+  })
+};
 
-const ProductPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+const ProductPage = ({data}) => {
+  const {frontmatter} = data.markdownRemark;
 
   return (
     <ProductPageTemplate
@@ -144,22 +110,22 @@ const ProductPage = ({ data }) => {
       fullImage={frontmatter.full_image}
       pricing={frontmatter.pricing}
     />
-  )
-}
+  );
+};
 
 ProductPage.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
+      frontmatter: PropTypes.object
+    })
+  })
+};
 
-export default ProductPage
+export default ProductPage;
 
 export const productPageQuery = graphql`
   query ProductPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
+    markdownRemark(id: {eq: $id}) {
       frontmatter {
         title
         image
@@ -207,4 +173,4 @@ export const productPageQuery = graphql`
       }
     }
   }
-`
+`;
